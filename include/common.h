36607b1b584b5b2cb10d3b1bdbd1522ae325e8ec
Fordi Malanda <fordimalanda7@gmail.com>
2026-06-30 08:23:46 +0200

    feat(config): declare ServerConfig structure and update lifecycle signatures for v1.0.4

--- include/common.h
+++ include/common.h
@@ -1,3 +1,8 @@
+/* * Copyright (c) 2026 Fordi / FomaDev. 
+ * Licensed under FomaDev Public License.
+ * See LICENSE file in the project root for full license information.
+ */
+
 #ifndef COMMON_H
 #define COMMON_H
 
@@ -8,11 +13,17 @@
 #include <stdbool.h>
 #include <errno.h>
 
-#define LITH_VERSION "1.0.3"
+#define LITH_VERSION "1.0.4"
 #define LITH_SERVER_NAME "LITH-v1"
 
 #define MAX_CONNECTIONS 100
 #define READ_TIMEOUT_SEC 5
+
+/* Structure pour stocker la configuration globale externe */
+typedef struct {
+    int port;
+    char public_dir[256];
+} ServerConfig;
 
 #define CHECK_ERROR(condition, msg) \
     do { \
--- include/server.h
+++ include/server.h
@@ -1,3 +1,8 @@
+/* * Copyright (c) 2026 Fordi / FomaDev. 
+ * Licensed under FomaDev Public License.
+ * See LICENSE file in the project root for full license information.
+ */
+
 #ifndef SERVER_H
 #define SERVER_H
 
@@ -23,6 +28,8 @@
 #include <string.h>
 #include <stdbool.h>
 
+#include "common.h" // Assure l'accès à ServerConfig
+
 #define DEFAULT_PORT 8090
 #define BACKLOG 10
 #define BUFFER_SIZE 4096
@@ -30,11 +37,13 @@
 typedef struct {
     socket_t client_socket;
     struct sockaddr_in client_address;
+    // Note optionnelle : vous pourrez y ajouter un pointeur vers la config si le handler en a besoin
 } ClientContext;
 
-// Déclarations des fonctions uniquement
-int lith_init_server(int port);
-void lith_start_server(int server_fd);
+/* Déclarations des fonctions de configuration et cycle de vie */
+int load_config(const char *filename, ServerConfig *config);
+int lith_init_server(const ServerConfig *config);
+void lith_start_server(int server_fd, const ServerConfig *config);
 void *lith_client_handler(void *arg);
 void lith_close_socket(socket_t s);
 
